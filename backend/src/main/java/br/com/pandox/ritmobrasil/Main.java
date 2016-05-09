524115427c2aacc2913504e9534bc530a1191bbb
matheus.messora <matheus.messora@netshoes.com>
2016-05-09 12:41:15 -0300

    backend

---
+++ backend/src/main/java/br/com/pandox/ritmobrasil/Main.java
@@ -0,0 +1,13 @@
+package br.com.pandox.ritmobrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class Main {
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+}
